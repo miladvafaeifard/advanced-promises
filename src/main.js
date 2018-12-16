b1d9be45ad765a604b78bae40457eaebbd3e2b05
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-12-16 01:33:22 +0100

    Make a sleep function that enhances for being able to see loading

--- src/main.js
+++ src/main.js
@@ -3,22 +3,29 @@ const starWarsUrl = "https://starwars.egghead.training/"
 const output = document.getElementById('output')
 const spinner = document.getElementById('spinner')
 
-const queryAPI = async(query) => {
+const queryAPI = async (query) => {
    const response = await fetch(starWarsUrl + query)
-   if(response.ok){
+   if (response.ok) {
       return response.json()
    } else {
       throw Error('Unsuccessful response')
    }
 }
 
-const main = async() => {
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+const main = async () => {
    try {
-      const [films, planets] = await Promise.all([
+      const [films, planets, species] = await Promise.all([
          queryAPI('films'),
          queryAPI('planets'),
+         queryAPI('species'),
+         // sleep(2000)
       ])
-      output.innerHTML = `${films.length} films and ${planets.length} planets`
+      output.innerHTML =
+         `${films.length} films, ` +
+         `${planets.length} planets, ` +
+         `${species.length} species`
    } catch (error) {
       console.log(error)
       output.innerHTML = ":("
